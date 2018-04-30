d832809dc5b255666f55ef0a7d4a02239b435541
Anthony <anth0ny0153@gmail.com>
2018-04-30 00:05:50 -0400

    First commit for Lab 6

---
+++ Lab6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sides = 0;
+            //task create an app that simulates dice rolling
+            Console.WriteLine("Do you want to roll the dice?(y/n)");
+            if (Console.ReadLine().ToLower() == "y")
+            {
+                Console.WriteLine("How many sides should each die have?");
+                sides = int.Parse(Console.ReadLine());
+                GenerateRandomNum(sides);
+
+
+                
+            }
+        }
+        public static int GenerateRandomNum(int diceSides)
+        {
+            Random rnd = new Random();
+            int randomNum = rnd.Next(1, diceSides);
+            int randomNum2 = rnd.Next(1, diceSides);
+            Console.WriteLine(randomNum);
+            Console.WriteLine(randomNum2);
+
+            return randomNum;
+        }
+    }
+}
