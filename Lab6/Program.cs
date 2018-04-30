0f0b8129924a56c119782dc98ba1daa84b53d949
Anthony <anth0ny0153@gmail.com>
2018-04-30 15:21:25 -0400

    fixed Lab6, added loop to give user choice to run program again

--- Lab6/Program.cs
+++ Lab6/Program.cs
@@ -10,28 +10,54 @@ namespace Lab6
     {
         static void Main(string[] args)
         {
-            int sides = 0;
-            //task create an app that simulates dice rolling
-            Console.WriteLine("Do you want to roll the dice?(y/n)");
-            if (Console.ReadLine().ToLower() == "y")
+            bool repeat = true;
+            while (repeat)
             {
-                Console.WriteLine("How many sides should each die have?");
-                sides = int.Parse(Console.ReadLine());
-                GenerateRandomNum(sides);
+                int sides = 0;
+                //task create an app that simulates dice rolling
+                Console.WriteLine("Do you want to roll the dice?(y/n)");
+                string answer = Console.ReadLine().ToLower();
+                if (answer == "y")
+                {
+                    Console.WriteLine("How many sides should each die have?");
+                    sides = int.Parse(Console.ReadLine());
+                    GenerateRandomNum(sides);
+                }
+                else if (answer == "n") ;
+                {
+                    repeat = false;
+                }
+                repeat = Confirm("Do you want to run the program again? (y/n): ");
+            }
+            Console.WriteLine("Bye!");
+        }
+        public static bool Confirm(string message)
+        {
+            Console.Write(message);
+            string input = Console.ReadLine();
+            if (input.ToLower() == "y") //continue
+            {
+                return true;
+            }
 
+            else if (input.ToLower() == "n") // ends program
+            {
+                return false;
+            }
 
-                
+            else
+            {
+                Console.WriteLine("Invalid Input");
+                return Confirm(message);
             }
         }
-        public static int GenerateRandomNum(int diceSides)
+        public static void GenerateRandomNum(int diceSides)
         {
             Random rnd = new Random();
-            int randomNum = rnd.Next(1, diceSides);
-            int randomNum2 = rnd.Next(1, diceSides);
+            int randomNum = rnd.Next(1, diceSides + 1);
+            int randomNum2 = rnd.Next(1, diceSides + 1);
             Console.WriteLine(randomNum);
             Console.WriteLine(randomNum2);
-
-            return randomNum;
         }
     }
 }
